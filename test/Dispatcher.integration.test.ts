f309564ac8654b02cc40b1b1049c9680cdcf68cb
Trevor Hanus <trevorhanus@gmail.com>
2017-09-14 18:47:52 -0700

    adds tests

--- test/Dispatcher.integration.test.ts
+++ test/Dispatcher.integration.test.ts
@@ -1,8 +1,8 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import * as sinon from 'sinon';
-import {SinonFakeTimers} from 'sinon';
-import {Dispatcher, IDispatcher} from "../src/Dispatcher";
-import {MockReversibleAction} from "./Mocks";
+import { SinonFakeTimers } from 'sinon';
+import { Dispatcher, IDispatcher } from "../src/Dispatcher";
+import { MockReversibleAction } from "./Mocks";
 
 describe('Dispatcher Integration', () => {
     let clock: SinonFakeTimers;
@@ -48,6 +48,33 @@ describe('Dispatcher Integration', () => {
             expect(secondAction.undo.callCount).to.equal(2);
             expect(firstAction.undo.callCount).to.equal(1);
         });
+
+        it('dispatch1>dispatch2>undo2>dispatch3>undo3>undo1', () => {
+            const action1 = new MockReversibleAction('test');
+            const action2 = new MockReversibleAction('test');
+
+            dispatcher.dispatch(action1);
+            dispatcher.dispatch(action2);
+
+            expect(action1.invoke.callCount).to.equal(1);
+            expect(action2.invoke.callCount).to.equal(1);
+
+            dispatcher.undo(); // undo 2
+
+            expect(action2.undo.callCount).to.equal(1);
+            expect(action1.undo.callCount).to.equal(0);
+
+            const action3 = new MockReversibleAction('test');
+            dispatcher.dispatch(action3);
+
+            dispatcher.undo(); // undo 3
+
+            expect(action3.undo.callCount).to.equal(1);
+
+            dispatcher.undo(); // undo 1
+
+            expect(action1.undo.callCount).to.equal(1);
+        });
     });
 
     describe('throttle actions', () => {
@@ -55,8 +82,8 @@ describe('Dispatcher Integration', () => {
             const action1_0 = new MockReversibleAction('test');
             const action2_0 = new MockReversibleAction('test2');
 
-            dispatcher.dispatch(action1_0, {throttle: 500});
-            dispatcher.dispatch(action2_0, {throttle: 500});
+            dispatcher.dispatch(action1_0, { throttle: 500 });
+            dispatcher.dispatch(action2_0, { throttle: 500 });
 
             clock.tick(100);
 
@@ -85,7 +112,7 @@ describe('Dispatcher Integration', () => {
     describe('debounce actions', () => {
         it('debounce 2 times', () => {
             const action0 = new MockReversibleAction('test');
-            dispatcher.dispatch(action0, {debounce: 500});
+            dispatcher.dispatch(action0, { debounce: 500 });
 
             expect(action0.invoke.callCount).to.equal(1);
             expect(dispatcher.canUndo).to.be.false;
@@ -93,7 +120,7 @@ describe('Dispatcher Integration', () => {
             clock.tick(100);
 
             const action1 = new MockReversibleAction('test');
-            dispatcher.dispatch(action1, {debounce: 500});
+            dispatcher.dispatch(action1, { debounce: 500 });
 
             expect(action1.invoke.callCount).to.equal(1);
             expect(dispatcher.canUndo).to.be.false;
